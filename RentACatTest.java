1f1d650f4241dc89771ed2797e5bd7f5fa52e5e8
Luke Gerhart <luke_gerhart@hotmail.com>
2017-10-08 12:58:16 -0400

    moved jars and wrote test for listCats

--- RentACatTest.java
+++ RentACatTest.java
@@ -1,8 +1,9 @@
 import static org.junit.Assert.*;
-
 import org.junit.*;
+import org.mockito.*;
+import java.util.ArrayList;
 
-public Class RentACatTest{
+public class RentACatTest{
 // Re-usable RentACat reference.
     RentACat _l;
 
@@ -16,6 +17,27 @@ public Class RentACatTest{
     // does not return a null reference
     @Test
     public void testRentACatExists() {
-	assertNotNull(_l);
+		assertNotNull(_l);
     }
+	
+	//test happy path
+	@Test
+	public void listCatsTest() {
+		ArrayList<Cat> cats = new ArrayList<Cat>();
+		Cat cat1 = Mockito.mock(Cat.class);
+		cat1._id = 1;
+		cat1._name = "cat1Name";
+		Cat cat2 = Mockito.mock(Cat.class);
+		cat2._id = 2;
+		cat2._name = "cat2Name";
+		Cat cat3 = Mockito.mock(Cat.class);
+		cat3._id = 3;
+		cat3._name = "cat3Name";
+		cats.add(cat1);
+		cats.add(cat2);
+		cats.add(cat3);
+		String list = _l.listCats(cats);
+		String correct = "ID 1. cat1Name\nID 2. cat2Name\nID 3. cat3Name";
+		assertEquals(list, correct);
+	}
 }
