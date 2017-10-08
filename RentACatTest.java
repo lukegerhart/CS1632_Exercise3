39fa9be0cdd00622953ca99e95986a5c4d43b868
Luke Gerhart <luke_gerhart@hotmail.com>
2017-10-08 13:12:35 -0400

    catExists happy path test

--- RentACatTest.java
+++ RentACatTest.java
@@ -62,4 +62,24 @@ public class RentACatTest{
 		String correct = "ID 2. cat2Name\nID 3. cat3Name";
 		assertEquals(list, correct);
 	}
+	
+	//test cat exists happy path
+	@Test
+	public void catExistsTest() {
+		ArrayList<Cat> cats = new ArrayList<Cat>();
+		Cat cat1 = Mockito.mock(Cat.class);
+		cat1._id = 1;
+		cat1._name = "cat1Name";
+		Cat cat2 = Mockito.mock(Cat.class);
+		cat2._id = 2;
+		cat2._name = "cat2Name";
+		Cat cat3 = Mockito.mock(Cat.class);
+		cat3._id = 3;
+		cat3._name = "cat3Name";
+		cats.add(cat1);
+		cats.add(cat2);
+		cats.add(cat3);
+		boolean catexists = _l.catExists(1, cats);
+		assertTrue(catexists);
+	}
 }
